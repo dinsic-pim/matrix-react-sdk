379c05f75bfd4f2f232d523f5ca140c57908b351
Léo Mora <yoshin@live.fr>
2019-02-27 16:38:09 +0100

    Remove multiple user on direct invitation

--- src/components/views/dialogs/AddressPickerDialog.js
+++ src/components/views/dialogs/AddressPickerDialog.js
@@ -55,6 +55,7 @@ module.exports = React.createClass({
         // Whether the current user should be included in the addresses returned. Only
         // applicable when pickerType is `user`. Default: false.
         includeSelf: PropTypes.bool,
+        invitationType: PropTypes.oneOf(['direct', 'room'])
     },
 
     getDefaultProps: function() {
@@ -64,6 +65,8 @@ module.exports = React.createClass({
             validAddressTypes: addressTypes,
             pickerType: 'user',
             includeSelf: false,
+            invitationType: 'direct',
+            textareaDisabled: null,
         };
     },
 
@@ -191,6 +194,7 @@ module.exports = React.createClass({
                 selectedList,
                 suggestedList: [],
                 query: "",
+                textareaDisabled: null
             });
             if (this._cancelThreepidLookup) this._cancelThreepidLookup();
         };
@@ -205,10 +209,12 @@ module.exports = React.createClass({
     onSelected: function(index) {
         const selectedList = this.state.selectedList.slice();
         selectedList.push(this.state.suggestedList[index]);
+        const textareaDisabledVal = this.props.invitationType === 'direct' ? true : null;
         this.setState({
             selectedList,
             suggestedList: [],
             query: "",
+            textareaDisabled: textareaDisabledVal
         });
         if (this._cancelThreepidLookup) this._cancelThreepidLookup();
     },
@@ -545,6 +551,10 @@ module.exports = React.createClass({
         }
 
         // Add the query at the end
+        // The use of 'disabled' and 'style' is a hack in order to prevent
+        // adding multiple peaople from the 1:1 creation window.
+        let invitationTypeStyle = {display:  this.state.textareaDisabled ? 'none' : 'inline' };
+
         query.push(
             <textarea key={this.state.selectedList.length}
                 rows="1"
@@ -554,7 +564,9 @@ module.exports = React.createClass({
                 onChange={this.onQueryChanged}
                 placeholder={this.props.placeholder}
                 defaultValue={this.props.value}
-                autoFocus={this.props.focus}>
+                autoFocus={this.props.focus}
+                disabled={this.state.textareaDisabled}
+                style={invitationTypeStyle}>
             </textarea>,
         );
 

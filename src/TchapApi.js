c39601d48322acbd5c1b665eb44b852888c579fe
Léo Mora <yoshin@live.fr>
2019-12-03 12:46:46 +0100

    Reorganizing Tchap Class

--- src/TchapApi.js
+++ src/TchapApi.js
@@ -1,6 +1,6 @@
 export default {
     hostBase: "https://matrix.",
-    info: "/_matrix/identity/api/v1/info?medium=email&address=",
+    infoFromEmailUrl: "/_matrix/identity/api/v1/info?medium=email&address=",
     publicKeyUrl: "/_matrix/media_proxy/unstable/public_key",
     scanEncryptedUrl: "/_matrix/media_proxy/unstable/scan_encrypted",
     scanUnencryptedUrl: "/_matrix/media_proxy/unstable/scan/",
@@ -8,7 +8,7 @@ export default {
     downloadEncryptedUrl: "/_matrix/media_proxy/unstable/download_encrypted",
     downloadUnencryptedThumbnailUrl: "/_matrix/media_proxy/unstable/thumbnail/",
     thumbnailParams: "?width=800&height=600&method=scale",
-    lookup: "/_matrix/client/unstable/account/3pid/lookup",
+    lookupUrl: "/_matrix/client/unstable/account/3pid/lookup",
     accountValidityResendEmailUrl: "/_matrix/client/unstable/account_validity/send_mail",
     passwordRulesUrl: "/_matrix/client/r0/password_policy",
 };

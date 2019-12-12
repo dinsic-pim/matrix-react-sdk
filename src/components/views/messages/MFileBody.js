678d76e5e37681ecc8c70e64c49bd88055f4fe52
Léo Mora <yoshin@live.fr>
2019-12-12 16:54:37 +0100

    Remove the origin param for thumbnails

--- src/components/views/messages/MFileBody.js
+++ src/components/views/messages/MFileBody.js
@@ -415,7 +415,6 @@ module.exports = React.createClass({
 
             // If the attachment is encryped then put the link inside an iframe.
             let renderer_url = MatrixClientPeg.get().getHomeserverUrl() + "/v1.html";
-            renderer_url += "?origin=" + encodeURIComponent(window.location.origin);
             return (
                 <span className="mx_MFileBody">
                     <div className="mx_MFileBody_download">
